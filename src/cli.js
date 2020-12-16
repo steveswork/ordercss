4f56e498501a47d3c2a004165de7a2cacad5f605
Stephen <stephen.isienyi@webkrafting.com>
2020-12-16 14:38:07 -0800

    refactoring cli logic and adding a common auto dev debug file

--- src/cli.js
+++ src/cli.js
@@ -6,32 +6,36 @@ import isEmpty from 'lodash.isempty';
 
 import { generateOutputModule, getCssImportOrder } from '.';
 
+export const runCli = entryModulePath => {
+	const realEntryPath = resolve( process.cwd(), entryModulePath );
+	const orderedCssPaths = getCssImportOrder( realEntryPath );
+	if( isEmpty( orderedCssPaths ) ) {
+		console.warn( chalk.yellow.bold(
+			'No compliantly commented CSS imports detected in the generated dependency graph.'
+		) );
+		return;
+	}
+	console.info( '\n\n%s:', chalk.blue.underline.bold( 'Ordered CSS Import Listing' ) );
+	console.log( orderedCssPaths.join( '\n' ) );
+	console.info( chalk.blue.bold( 'Listing completed.' ) );
+	try {
+		generateOutputModule( orderedCssPaths, realEntryPath );
+		console.log( chalk.green.bold( 'Module update completed.' ) );
+	} catch ( e ) {
+		console.log( '\n\n%s', chalk.yellow.bold( 'Unsuccessful module update attempt.' ) );
+		console.error( chalk.red.bold( e.message ) );
+		console.log( e );
+	}
+	console.log( '%s\n\n', chalk.bold( 'DONE.' ) );
+};
+
 const cli = () => {
 	const entryModulePath = process.argv[ 2 ];
 	try {
 		if( isEmpty( entryModulePath ) ) {
 			throw new TypeError( 'No entry module path supplied.' );
 		}
-		const realEntryPath = resolve( process.cwd(), entryModulePath );
-		const orderedCssPaths = getCssImportOrder( realEntryPath );
-		if( isEmpty( orderedCssPaths ) ) {
-			console.warn( chalk.yellow.bold(
-				'No compliantly commented CSS imports detected in the generated dependency graph.'
-			) );
-			return;
-		}
-		console.info( '\n\n%s:', chalk.blue.underline.bold( 'Ordered CSS Import Listing' ) );
-		console.log( orderedCssPaths.join( '\n' ) );
-		console.info( chalk.blue.bold( 'Listing completed.' ) );
-		try {
-			generateOutputModule( orderedCssPaths, realEntryPath );
-			console.log( chalk.green.bold( 'Module update completed.' ) );
-		} catch ( e ) {
-			console.log( '\n\n%s', chalk.yellow.bold( 'Unsuccessful module update attempt.' ) );
-			console.error( chalk.red.bold( e.message ) );
-			console.log( e );
-		}
-		console.log( '%s\n\n', chalk.bold( 'DONE.' ) );
+		runCli( entryModulePath );
 	} catch( e ) {
 		console.error( chalk.red.bold( e.message ) );
 		console.log( e );
